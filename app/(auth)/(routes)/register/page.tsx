a5a4ab0927d44d5dda1501ace5b97155c147a679
Abdul Musavveer Alji <abdulmusavveeralji@gmail.com>
2023-10-24 22:23:27 +0530

    folder setup

---
+++ app/(auth)/(routes)/register/page.tsx
@@ -0,0 +1,5 @@
+const RegisterPage = () => {
+    return ( <div className="">Register Page</div> );
+}
+ 
+export default RegisterPage;
--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div className="">
-      <p className='text-3xl font-bold text-indigo-500'>Hello Discord Clone</p>
+      <p className='text-3xl font-bold text-indigo-500'>This is a protected route.</p>
       <Button variant="outline">click me</Button>
     </div>
   )
